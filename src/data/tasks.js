18370eb45c5690f640d6c4ced73fdbd0d284ce4f
HenryD5 <david_yamunaque_5@outlook.com>
2023-05-07 15:09:15 -0500

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "title 1",
+    description: "des....s d d  d s ",
+  },
+  {
+    id: 1,
+    title: "title 2",
+    description: "des....s d d  d s ",
+  },
+  {
+    id: 2,
+    title: "title 3",
+    description: "des....s d d  d s ",
+  },
+];
